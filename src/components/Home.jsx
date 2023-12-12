227470a94b6df449a687214bb5a0d405fcccbfe8
Michael <mickaellunzola@gmail.com>
2023-12-12 13:06:54 +0100

    structuration project and add a feat

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import UserContext from "../context/userContext.jsx";
+import data from "../models/services/DataService.js";
+import Post from "./Post.jsx";
+
+export default function Home() {
+    const user = useContext(UserContext);
+
+    return (
+        <>
+            <header>
+                <h1>Forum instantané</h1>
+            </header>
+            <main>
+                <h2>👋 Bienvenue, {user.pseudo}</h2>
+                {data.map((e) => (
+                    <Post post={e} />
+                ))}
+            </main>
+        </>
+    );
+}
